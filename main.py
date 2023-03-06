7aebbdf6b3b37166aed31557e832c7f3e1f8c376
Alina <alinatussupova@gmail.com>
2023-03-06 13:16:01 +0600

    First Commit.

--- main.py
+++ main.py
@@ -321,8 +321,9 @@ class AboutDialog(QMessageBox):
         self.setText(content)
    
 
-app = QApplication(sys.argv)
-main_window = MainWindow()
-main_window.show()
-main_window.load_data()
-sys.exit(app.exec())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    main_window.load_data()
+    sys.exit(app.exec())
